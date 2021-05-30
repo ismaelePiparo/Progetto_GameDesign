dfd9e75707b3a072cd4fbb1e5ec148a4108e050b
Ismaele <ismaelepiparo@gmail.com>
2021-05-30 18:03:34 +0200

    Sisteamati script

--- Progetto Game Design/Assets/Scripts/AnimatedThirdPController.cs
+++ Progetto Game Design/Assets/Scripts/AnimatedThirdPController.cs
@@ -31,57 +31,65 @@ public class AnimatedThirdPController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("_isRun: " + _isRun);
-        //Handle the Input
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        _inputVector = new Vector3(h, 0, v);
-        _inputSpeed = Mathf.Clamp(_inputVector.magnitude, 0f, 1f);
 
-        UpdateAnimations();
-
-        //Compute direction According to Camera Orientation
-        _targetDirection = _cameraT.TransformDirection(_inputVector).normalized;
-        _targetDirection.y = 0f;
-
-        if (_inputSpeed <= 0f || _isJumping)
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Play_Flute"))
         {
-            _isRun = false;
-            return;
+            Debug.Log("Sto suonado");
         }
-
-
-        //Calculate rotation vector and rotate
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, _targetDirection, _rotationSpeed * Time.deltaTime, 0f);
-        transform.rotation = Quaternion.LookRotation(newDir);
-
-        //Translate along forward
-        transform.Translate(transform.forward * _inputSpeed * _speed * Time.deltaTime, Space.World);
-
-        Debug.DrawRay(transform.position + transform.up * 3f, _targetDirection * 5f, Color.red);
-        Debug.DrawRay(transform.position + transform.up * 3f, newDir * 5f, Color.blue);
-
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S))
+        else
         {
-            if (Time.time - lastTime < 0.2f)
+
+            Debug.Log("_isRun: " + _isRun);
+            //Handle the Input
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            _inputVector = new Vector3(h, 0, v);
+            _inputSpeed = Mathf.Clamp(_inputVector.magnitude, 0f, 1f);
+
+            UpdateAnimations();
+
+            //Compute direction According to Camera Orientation
+            _targetDirection = _cameraT.TransformDirection(_inputVector).normalized;
+            _targetDirection.y = 0f;
+
+            if (_inputSpeed <= 0f || _isJumping)
             {
-                lastTime = Time.time;
-                _speed = _runSpeed;
-                _isRun = true;
-                Debug.Log("dubletap");
-                // turn on running
-            }
-            else
-            {
-                lastTime = Time.time;
                 _isRun = false;
-                _speed = _defaultSpeed;
-                Debug.Log("singletap");
-                // turn on (or switch to) walking
+                return;
             }
 
-        }
 
+            //Calculate rotation vector and rotate
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, _targetDirection, _rotationSpeed * Time.deltaTime, 0f);
+            transform.rotation = Quaternion.LookRotation(newDir);
+
+            //Translate along forward
+            transform.Translate(transform.forward * _inputSpeed * _speed * Time.deltaTime, Space.World);
+
+            Debug.DrawRay(transform.position + transform.up * 3f, _targetDirection * 5f, Color.red);
+            Debug.DrawRay(transform.position + transform.up * 3f, newDir * 5f, Color.blue);
+
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S))
+            {
+                if (Time.time - lastTime < 0.2f)
+                {
+                    lastTime = Time.time;
+                    _speed = _runSpeed;
+                    _isRun = true;
+                    Debug.Log("dubletap");
+                    // turn on running
+                }
+                else
+                {
+                    lastTime = Time.time;
+                    _isRun = false;
+                    _speed = _defaultSpeed;
+                    Debug.Log("singletap");
+                    // turn on (or switch to) walking
+                }
+
+            }
+        }
 
     }
 
